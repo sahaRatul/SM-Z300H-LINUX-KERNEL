9a3695dc4864601483adbc6979492292a9913278
Ratul Saha <ratul.saha@cognizant.com>
2018-08-14 18:45:10 +0530

    Return address build error fixed

--- arch/x86/include/generated/asm/.syscalls_32.h.cmd
+++
@@ -1,1 +0,0 @@
-cmd_arch/x86/syscalls/../include/generated/asm/syscalls_32.h := /bin/bash '/home/ratul/Desktop/Source/source/linux-3.10-sc7730/arch/x86/syscalls/syscalltbl.sh' /home/ratul/Desktop/Source/source/linux-3.10-sc7730/arch/x86/syscalls/syscall_32.tbl arch/x86/syscalls/../include/generated/asm/syscalls_32.h
